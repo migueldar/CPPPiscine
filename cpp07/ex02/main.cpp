ca93d8b945d6ae2a421020bd5ee2f7234c24f64b
migueldar <miguelarp3907@gmail.com>
2024-08-15 16:20:55 +0200

    ready

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include "Array.hpp"
 
-// void leaks() { system("leaks array"); }
-
 #define MAX_VAL 750
 int main(int, char**)
 {
-	//atexit(leaks);
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
